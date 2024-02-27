037ea7666811a6a0998b61fd10b0b5f3b082a9e8
ryuh2929 <ryuh2929@gmail.com>
2024-02-27 21:52:15 +0900

    1번 과제 조건은 다 만족했는데 더 다듬어야할지... 일단은 다른 문제 다 하고 다시 해야할듯

--- 1.upDownGame.py
+++ 1.upDownGame.py
@@ -1,5 +1,35 @@
-a = 1
-b = 2
-c = a.__add__(b)
-print(c)
-print(a.__sub__(b))
+import random
+
+number = int(input("1~100 사이의 숫자를 입력하세요: "))
+
+rand_num = random.randint(1, 100)
+
+print(rand_num)
+
+repeat_count = 1
+
+while repeat_count < 100:
+    if number < 1 or number > 100:
+        print('1에서 100사이의 숫자만 가능합니다')
+        number = int(input("1~100 사이의 숫자를 입력하세요: "))
+        continue
+    elif number == rand_num:
+        print('정답입니다')
+        print(f'{repeat_count}번만에 맞추셨습니다')
+        yes_or_no = input("다시 플레이하시겠습니까?(Y/N): ")
+        if yes_or_no == 'Y' or yes_or_no == 'y':
+            number = int(input("1~100 사이의 숫자를 입력하세요: "))
+            rand_num = random.randint(1, 100)
+            print(rand_num)
+            repeat_count = 1
+            continue
+        else:
+            print('게임을 끝냅니다')
+            break
+    elif number > rand_num:
+        print('DOWN')
+    elif number < rand_num:
+        print('UP')
+    repeat_count += 1
+    number = int(input("1~100 사이의 숫자를 입력하세요: "))
+    continue
