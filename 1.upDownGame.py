1e1936d3bd6fcdeff329f4154265d28f0e3e12b5
ryuh2929 <ryuh2929@gmail.com>
2024-02-28 09:04:40 +0900

    도전과제 추가작성

--- 1.upDownGame.py
+++ 1.upDownGame.py
@@ -4,9 +4,9 @@ number = int(input("1~100 사이의 숫자를 입력하세요: "))
 
 rand_num = random.randint(1, 100)
 
-print(rand_num)
-
 repeat_count = 1
+
+best_record = 100
 
 while repeat_count < 100:
     if number < 1 or number > 100:
@@ -16,6 +16,8 @@ while repeat_count < 100:
     elif number == rand_num:
         print('정답입니다')
         print(f'{repeat_count}번만에 맞추셨습니다')
+        if repeat_count < best_record:
+            best_record = repeat_count
         yes_or_no = input("다시 플레이하시겠습니까?(Y/N): ")
         if yes_or_no == 'Y' or yes_or_no == 'y':
             number = int(input("1~100 사이의 숫자를 입력하세요: "))
@@ -24,7 +26,8 @@ while repeat_count < 100:
             repeat_count = 1
             continue
         else:
-            print('게임을 끝냅니다')
+            print('게임을 끝냅니다') 
+            print(f'최고기록은 {best_record}번 입니다')
             break
     elif number > rand_num:
         print('DOWN')
